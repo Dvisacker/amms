988db606f27f7adb60de00d0441af2f5377d57b2
dvisacker <davidvanisacker@gmail.com>
2024-11-28 17:19:17 +0900

    fix - use correct ve33 factory interface

--- contracts/src/GetVe33PoolsBatchRequest.sol
+++ contracts/src/GetVe33PoolsBatchRequest.sol
@@ -2,32 +2,28 @@
 pragma solidity ^0.8.0;
 
 interface IFactory {
-    function allPairs(uint256 idx) external returns (address);
+    function allPools(uint256 idx) external returns (address);
 }
 
 /**
- @dev This contract is not meant to be deployed. Instead, use a static call with the
-      deployment bytecode as payload.
+ * @dev This contract is not meant to be deployed. Instead, use a static call with the
+ *       deployment bytecode as payload.
  */
-contract GetUniswapV2PairsBatchRequest {
-    constructor(
-        uint256 from,
-        uint256 step,
-        address factory
-    ) {
+contract GetVe33PoolsBatchRequest {
+    constructor(uint256 from, uint256 step, address factory) {
         uint256 distance = step - from;
 
         // There is a max number of pool as a too big returned data times out the rpc
-        address[] memory allPairs = new address[](distance);
+        address[] memory allPools = new address[](distance);
 
-        // Query every pool balance
+        // Query every pool address
         for (uint256 i = 0; i < distance; i++) {
-            allPairs[i] = IFactory(factory).allPairs(from + i);
+            allPools[i] = IFactory(factory).allPools(from + i);
         }
 
         // ensure abi encoding, not needed here but increase reusability for different return types
         // note: abi.encode add a first 32 bytes word with the address of the original data
-        bytes memory _abiEncodedData = abi.encode(allPairs);
+        bytes memory _abiEncodedData = abi.encode(allPools);
 
         assembly {
             // Return from the start of the data (discarding the original data address)
